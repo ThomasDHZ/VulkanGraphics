46a48b042311036875daa8ee79826a8a36feed69
ZZT <ZZT@DESKTOP-254MDLQ>
2020-04-30 19:01:35 -0400

    z

--- VulkanGraphics/VulkanGraphics.cpp
+++ VulkanGraphics/VulkanGraphics.cpp
@@ -85,6 +85,7 @@ void VulkanGraphics::Update(uint32_t NextFrameIndex)
 	for (auto mesh : MeshList)
 	{
 		UniformBufferObject ubo{};
+		ubo.model = mat4
 		ubo.model = glm::rotate(glm::mat4(1.0f), time * glm::radians(90.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 		ubo.view = glm::lookAt(glm::vec3(2.0f, 2.0f, 2.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
 		ubo.proj = glm::perspective(glm::radians(45.0f), GetSwapChainResolution(renderer)->width / (float)GetSwapChainResolution(renderer)->height, 0.1f, 10.0f);
